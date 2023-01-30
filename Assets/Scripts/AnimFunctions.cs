ab6959f14da3edd98c310c4192596056e1edf7ba
Garlicsuter <markasuter@gmail.com>
2023-01-30 07:34:13 -0500

    simple animation done

---
+++ Assets/Scripts/AnimFunctions.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimFunctions : MonoBehaviour
+{
+    public void DebugMe()
+    {
+        Debug.Log("You ran an event!");
+    }
+
+    public void UprockDebugMe()
+    {
+        Debug.Log("Uprock event ran!");
+    }
+}
